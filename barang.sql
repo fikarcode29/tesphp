81e9577d5f096646d927bc104dc48c4bbb3ea034
fikarcode29 <148299463+fikarcode29@users.noreply.github.com>
2023-10-18 10:29:13 +0700

    Add files via upload

---
+++ barang.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE barang_db;
+USE barang_db;
+CREATE TABLE `barang` (
+`id_barang` int(11) NOT NULL,
+`nama_barang` varchar(111) NOT NULL,
+`harga_barang` int(11) NOT NULL,
+`stok_barang` int(11) NOT NULL
+) 
